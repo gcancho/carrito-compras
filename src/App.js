4630b2d4101e26da63846fe0371d2320416c52fd
gcancho <giovannicb19@gmail.com>
2022-01-17 17:28:40 -0500

    add: Componentes basicos y rutas de la aplicacion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import { NavLink, Route, Routes } from "react-router-dom";
+import Inicio from "./componentes/Inicio";
+import Blog from "./componentes/Blog";
+import Tienda from "./componentes/Tienda";
+import Error404 from "./componentes/Error404";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Menu>
+        <NavLink to="/">Inicio</NavLink>
+        <NavLink to="/Blog">Blog</NavLink>
+        <NavLink to="/Tienda">Tienda</NavLink>
+      </Menu>
+      <main>
+        {/* Con la actualizacion nueva, los 'Switch' se remplazan por los 'Routes' */}
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/Tienda" element={<Tienda />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </main>
+      <aside></aside>
+    </Contenedor>
   );
-}
+};
+
+const Contenedor = styled.div`
+  max-width: 1000px;
+  padding: 40px;
+  width: 90%;
+  display: grid;
+  gap: 20px;
+  grid-template-columns: 2fr 1fr;
+  background: #fff;
+  margin: 40px 0;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
+
+const Menu = styled.nav`
+  width: 100%;
+  text-align: center;
+  background: #092c4c;
+  grid-column: span 2;
+  border-radius: 3px;
+
+  a {
+    color: #fff;
+    display: inline-block;
+    padding: 15px 20px;
+  }
+
+  a:hover {
+    background: #1d85e8;
+    text-decoration: none;
+  }
+`;
 
 export default App;
---
+++ src/componentes/Error404.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Error404 = () => {
+  return (
+    <div>
+      <h1>Error404</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        eaque voluptatem quibusdam culpa dicta rem nobis, corporis recusandae
+        maiores doloremque nihil, at maxime aliquid excepturi, a soluta deserunt
+        aperiam veniam.
+      </p>
+    </div>
+  );
+};
+
+export default Error404;
